d34a13991f2d7580c97eb6e932ac649eb50da991
rissins <tnalsqqq@naver.com>
2023-01-08 23:29:57 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const repository = "nft-jsfront";
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  // assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
   images: {loader: 'akamai', path: '/'}
 };
