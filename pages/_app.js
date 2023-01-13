cefc399f9d033fc5cbf154506be38772904f14df
rissins <tnalsqqq@naver.com>
2023-01-13 22:45:51 +0900

    style: 불필요 주석 제거

--- pages/_app.js
+++ pages/_app.js
@@ -2,36 +2,12 @@ import '../styles/globals.css'
 import { PortfolioProvider } from "../context/context";
 import { prefix } from "../config/config";
 
-// export default function App({ Component, pageProps }) {
-//   return <Component {...pageProps} />
-// }
-
-// function App() {
-//     return (
-//         <PortfolioProvider value={ { prefix } }>
-//             {/*<Header />*/}
-//             {/*<Home />*/}
-//             {/*<About />*/}
-//             {/*<Skills />*/}
-//             {/*<Projects />*/}
-//             {/*<Career />*/}
-//             {/*<Footer />*/}
-//         </PortfolioProvider>
-//     );
-// }
-// export default App;
-
 
 function App({ Component, pageProps }) {
     return (
         // @ts-ignore
         <PortfolioProvider value={{ prefix }}>
-            {/*<RecoilRoot>*/}
-            {/*    <Global styles={globalStyles} />*/}
-            {/*    <Layout>*/}
                     <Component {...pageProps} />
-            {/*    </Layout>*/}
-            {/*</RecoilRoot>*/}
         </PortfolioProvider>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,16 @@
-// import Layout from '../components/Layout';
-import Link from 'next/link';
 import React from 'react';
 
 import axios from 'axios';
 import { prefix } from "../config/config";
 import style from '../styles/Search.module.css';
-import router from 'next/router'
-import Image from "next/image";
 
 const Index = (props) => (
     <main>
       <div className={style.search}>
         <input type="text" id="search-name" placeholder="검색어 입력" className={style.input}/>
         <img id="search-button"
-            // src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"
              src={`${prefix}/images/search.png`}
              className={style.img}
-            // th:onclick="searchClick()"aa
              onClick={searchClick}
              alt="13"/>
         <ul id="search-list">
@@ -38,8 +32,6 @@ async function searchClick() {
     li.innerHTML +=
         "<ul>" +
         "<li>" + data[i].word + "/" + data[i].count + "</li>" +
-        // "<li> " + json[i].count + "</>" +
-        // "<td> " + json[i].body + "</td>" +
         "</ul>";
   }
 
@@ -49,26 +41,5 @@ async function searchClick() {
     data: data
   }
 }
-
-// Index.getInitialProps = async function() {
-//     const res = await axios.get('https://5nft.link/search?word=개죽')
-//     const data = await res.data;
-//     const li =  await document.getElementById('search-list');
-//
-//     for (let i = 0; i < data.length; i++) {
-//         li.innerHTML +=
-//             "<ul>" +
-//             "<li>" + data[i].word + "/" + data[i].count + "</li>" +
-//             // "<li> " + json[i].count + "</>" +
-//             // "<td> " + json[i].body + "</td>" +
-//             "</ul>";
-//     }
-//
-//     console.log(`Show data fetched. Count: ${data.length}`);
-//
-//     return {
-//         data: data
-//     }
-// };
 
 export default Index
